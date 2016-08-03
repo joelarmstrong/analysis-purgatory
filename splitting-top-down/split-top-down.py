87e656ee10a3260e9df385f172c6484b26dce096
Joel Armstrong <jcarmstr@ucsc.edu>
2016-08-03 16:03:00 -0700

    build trees top-down

--- splitting-top-down/split-top-down.py
+++ splitting-top-down/split-top-down.py
@@ -3,11 +3,18 @@
 Testing the accuracy of a top-down clustering method using k-means
 clustering, k = 2, for splitting homologous blocks into
 reference-orthologous blocks.
+
+Requires:
+- scikit-learn
+- kmodes
+- biopython
+- sonLib
 """
 from argparse import ArgumentParser
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import OneHotEncoder
 from kmodes.kmodes import KModes
+from Bio import Phylo
 import numpy as np
 from sonLib.bioio import fastaRead
 
@@ -56,7 +63,7 @@ def parse_args():
     parser = ArgumentParser(description=__doc__)
     parser.add_argument('fasta', help='fasta file (all sequences must have the '
                         'same length')
-    parser.add_argument('species-tree', help='species tree')
+    parser.add_argument('species_tree', help='species tree (newick format)')
     parser.add_argument('reference', help='reference species')
     parser.add_argument('--cluster-method',
                         choices=['k-means', 'k-modes'],
@@ -72,14 +79,39 @@ def cluster_matrix(matrix, cluster_method):
     else:
         raise ArgumentError('Unknown cluster method: %s' % cluster_method)
 
+def build_tree_topdown(columns, seq_names, cluster_method):
+    def is_finished(cluster, columns):
+        return len(cluster) <= 2 or all([len(set(column)) == 1 for column in columns])
+    def recurse(columns, seq_names):
+        matrix = columns_to_matrix(columns, one_hot_encode=(cluster_method == 'k-means'))
+        cluster_assignments = cluster_matrix(matrix, cluster_method)
+        cluster0_indices = [i for i, cluster in enumerate(cluster_assignments) if cluster == 0]
+        cluster1_indices = [i for i, cluster in enumerate(cluster_assignments) if cluster == 1]
+        cluster0 = [seq_names[i] for i in cluster0_indices]
+        cluster1 = [seq_names[i] for i in cluster1_indices]
+        cluster0_columns = [[column[i] for i in cluster0_indices] for column in columns]
+        if is_finished(cluster0, cluster0_columns):
+            clade0 = Phylo.BaseTree.Clade(clades=map(lambda x: Phylo.BaseTree.Clade(name=x), cluster0))
+        else:
+            clade0 = Phylo.BaseTree.Clade(clades=recurse(cluster0_columns, cluster0))
+
+        cluster1_columns = [[column[i] for i in cluster1_indices] for column in columns]
+        if is_finished(cluster1, cluster1_columns):
+            clade1 = Phylo.BaseTree.Clade(clades=map(lambda x: Phylo.BaseTree.Clade(name=x), cluster1))
+        else:
+            clade1 = Phylo.BaseTree.Clade(clades=recurse(cluster1_columns, cluster1))
+        return (clade0, clade1)
+    tree = Phylo.BaseTree.Tree(Phylo.BaseTree.Clade(clades=recurse(columns, seq_names)))
+    return tree
+
 def main():
     args = parse_args()
+    species_tree = Phylo.parse(args.species_tree, 'newick')
     seqs = dict(fastaRead(args.fasta))
     seq_names = seqs.keys()
-    print repr(seq_names)
     cols = seqs_to_columns(seqs, seq_names)
-    matrix = columns_to_matrix(cols, one_hot_encode=(args.cluster_method == 'k-means'))
-    print cluster_matrix(matrix, args.cluster_method)
+    tree = build_tree_topdown(cols, seq_names, args.cluster_method)
+    Phylo.draw_ascii(tree)
 
 if __name__ == '__main__':
     main()
