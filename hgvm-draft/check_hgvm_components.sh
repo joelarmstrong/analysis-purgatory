38e9dae3fc1bb85d4a3831f6b5808139e09687d0
Joel Armstrong <jcarmstr@ucsc.edu>
2017-02-16 11:38:00 -0800

    Update checking script to check 1 level of transitivity

--- hgvm-draft/check_hgvm_components.sh
+++ hgvm-draft/check_hgvm_components.sh
@@ -34,9 +34,26 @@ for chr in $(halStats --sequences $GENOME $HAL_FILE); do
     # uncomment to show an unexpected input will actually trigger failure
     #echo "chr22_GL383582v2 3030 3101022030" >>$OUT_FILE
     if egrep -v "^(\w+_alt|$chr)\s" $OUT_FILE >$MYTMP/failure.out; then
-        echo "FAILURE on lifting from $chr."
+        echo "FAILURE on initial lifting from $chr."
         cat $MYTMP/failure.out
         exit 1
     fi
+    for mappedChr in $(cut -f 1 $OUT_FILE | egrep -v "^$chr$" | sort | uniq | tee test.txt | tr '\n' ','); do
+        echo "Checking mapped chr $mappedChr"
+        IN_FILE=$MYTMP/in.bed
+        halStats --bedSequences $GENOME $HAL_FILE | egrep "^$mappedChr\s" > $IN_FILE
+        # Lift up
+        PARENT=$(halStats --parent $GENOME $HAL_FILE)
+        PARENT_FILE=$MYTMP/parent.bed
+        halLiftover $HAL_FILE $GENOME $IN_FILE $PARENT $PARENT_FILE
+        # Lift back down
+        OUT_FILE=$MYTMP/out.bed
+        halLiftover $HAL_FILE $PARENT $PARENT_FILE $GENOME $OUT_FILE
+        if egrep -v "^(\w+_alt|$chr)\s" $OUT_FILE >$MYTMP/failure.out; then
+            echo "FAILURE on a secondary lifting from mapped chr $mappedChr."
+            cat $MYTMP/failure.out
+            exit 1
+        fi
+    done
 done
 rm -fr $MYTMP
