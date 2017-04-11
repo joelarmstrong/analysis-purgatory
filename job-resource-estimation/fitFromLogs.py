7c1aef6bb953f6a292cc4ad50728394a5c43207a
Joel Armstrong <jcarmstr@ucsc.edu>
2017-04-11 15:44:38 -0700

    Alter resource estimation to take multiple features
    This is actually sort of unneeded now, because the jobs have been
    shrunk enough that the fit doesn't matter as much.

--- job-resource-estimation/fitFromLogs.py
+++ job-resource-estimation/fitFromLogs.py
@@ -6,53 +6,54 @@ given a set of toil logs.
 By default, it generates a quadratic fit.
 """
 import re
+import json
 from argparse import ArgumentParser
+from collections import defaultdict
 
-import pandas as pd
 import numpy as np
-import seaborn as sns
+from sklearn import linear_model
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.metrics import mean_squared_error
 
 def parse_args():
     parser = ArgumentParser(description=__doc__)
     parser.add_argument('logFile')
-    parser.add_argument('--degree', type=int, default=2)
     parser.add_argument('--headroom', help='Add an extra x ratio of headroom',
                         type=float, default=0.1)
     args = parser.parse_args()
     return args
 
-def evaluate(poly, x):
-    """Evaluates a polynomial (provided as a pd.Series) at x."""
-    X = [x**degree for degree in reversed(xrange(len(poly)))]
-    return sum(poly*X)
-
 def main():
     args = parse_args()
-    data = []
+    jobData = defaultdict(list)
     with open(args.logFile) as f:
         for line in f:
-            match = re.search(r'Max memory used for job (.+) on input size ([0-9]+): ([0-9]+)', line)
+            match = re.search(r'Max memory used for job (.+) on JSON features (.+): ([0-9]+)', line)
             assert match is not None
-            datum = { 'Length': int(match.group(2)),
-                      match.group(1): int(match.group(3)) }
+            datum = json.loads(match.group(2))
+            # Now that we shrunk things, we only (probably) care about one feature
+            datum = {k: datum[k] for k in datum if k in ['MaxMemory', 'maxFlowerSize']}
             # Give it extra headroom
-            datum[match.group(1)] += datum[match.group(1)] * args.headroom
-            data.append(datum)
+            datum['MaxMemory'] = int(match.group(3)) * (1.0 + args.headroom)
+            job = match.group(1)
+            jobData[job].append(datum)
 
-    df = pd.DataFrame(data)
-    for col in df:
-        if col not in ['Length']:
-            df2 = df[['Length', col]].dropna(axis=0)
-            poly = np.polyfit(df2['Length'], df2[col], args.degree)
-            print poly
-            print col
-            sns.regplot(data=df2, x='Length', y=col, order=args.degree, ci=None)
-            predicted = np.polyval(poly, df2['Length'])
-            residuals = df2[col] - predicted
-            # Plot residuals
-            df3 = pd.DataFrame({'predicted':predicted, 'residuals': residuals})
-            sns.jointplot(data=df3, x='predicted', y='residuals')
-            sns.plt.show()
+    for job, data in jobData.iteritems():
+        vec = DictVectorizer()
+        y = np.array([d.pop('MaxMemory') for d in data])
+        X = vec.fit_transform(data)
+        lm = linear_model.LinearRegression()
+        lm.fit(X, y)
+        # y without headroom adjustment
+        true_y = y / (1.0 + args.headroom)
+        reprediction = lm.predict(X)
+        mse = mean_squared_error(true_y, reprediction)
+        max_overestimate = max(reprediction - true_y)
+        max_underestimate = max(true_y - reprediction)
+        print 'Job %s: r^2: %s MSE: %s max overestimate: %sGiB max underestimate: %sGiB coef: %s intercept: %s' % \
+            (job, lm.score(X, y), mse,
+             max_overestimate/(1024*1024*1024), max_underestimate/(1024*1024*1024),
+             dict(zip(vec.get_feature_names(), lm.coef_)), lm.intercept_)
 
 if __name__ == '__main__':
     main()
