d040303ef50c52c23f9a64e154c07e1ce8a849e5
Joel Armstrong <jcarmstr@ucsc.edu>
2017-05-23 20:30:42 -0700

    Fit using convex optimization that never underestimates training data

--- job-resource-estimation/fitFromLogs.py
+++ job-resource-estimation/fitFromLogs.py
@@ -11,9 +11,23 @@ from argparse import ArgumentParser
 from collections import defaultdict
 
 import numpy as np
-from sklearn import linear_model
+import cvxpy
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.metrics import mean_squared_error
+
+def fit_lm_never_underestimate(X, y):
+    """Fit a linear model to the data, subject to the constraint that the
+    linear model never underestimates the training points."""
+    n = X.shape[1]    # Number of features
+    m = X.shape[0]    # Number of examples
+    B = cvxpy.Variable(n)
+    # Must always be above the training points
+    constraints = [(X*B)[i] >= y[i] for i in xrange(m)]
+    # Minimize absolute error
+    objective = cvxpy.Minimize(cvxpy.sum_entries(X*B - y))
+    problem = cvxpy.Problem(objective, constraints)
+    problem.solve()
+    return B.value
 
 def parse_args():
     parser = ArgumentParser(description=__doc__)
@@ -30,30 +44,31 @@ def main():
         for line in f:
             match = re.search(r'Max memory used for job (.+) on JSON features (.+): ([0-9]+)', line)
             assert match is not None
+            job = match.group(1)
             datum = json.loads(match.group(2))
             # Now that we shrunk things, we only (probably) care about one feature
             datum = {k: datum[k] for k in datum if k in ['MaxMemory', 'maxFlowerSize']}
             # Give it extra headroom
             datum['MaxMemory'] = int(match.group(3)) * (1.0 + args.headroom)
-            job = match.group(1)
             jobData[job].append(datum)
 
     for job, data in jobData.iteritems():
-        vec = DictVectorizer()
+        vec = DictVectorizer(sparse=False)
         y = np.array([d.pop('MaxMemory') for d in data])
         X = vec.fit_transform(data)
-        lm = linear_model.LinearRegression()
-        lm.fit(X, y)
+        # Extend the data so we can fit an intercept
+        X = np.concatenate([np.ones((X.shape[0], 1)), X], axis=1)
+        B = fit_lm_never_underestimate(X, y)
         # y without headroom adjustment
-        true_y = y / (1.0 + args.headroom)
-        reprediction = lm.predict(X)
+        reprediction = X.dot(B)
+        true_y = (y / (1.0 + args.headroom)).reshape(reprediction.shape)
         mse = mean_squared_error(true_y, reprediction)
         max_overestimate = max(reprediction - true_y)
         max_underestimate = max(true_y - reprediction)
-        print 'Job %s: r^2: %s MSE: %s max overestimate: %sGiB max underestimate: %sGiB coef: %s intercept: %s' % \
-            (job, lm.score(X, y), mse,
+        print 'Job %s: MSE: %s max overestimate: %sGiB max underestimate: %sGiB coef: %s' % \
+            (job, mse,
              max_overestimate/(1024*1024*1024), max_underestimate/(1024*1024*1024),
-             dict(zip(vec.get_feature_names(), lm.coef_)), lm.intercept_)
+             dict(zip(['intercept'] + vec.get_feature_names(), B)))
 
 if __name__ == '__main__':
     main()
