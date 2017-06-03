078f1e03c759f6de45415e58ce378922aeb44e44
Joel Armstrong <jcarmstr@ucsc.edu>
2017-06-03 16:01:39 -0700

    Keep track of whether the k-mers fall within a masked region

--- preprocessor/spacedKmerPreprocessor.py
+++ preprocessor/spacedKmerPreprocessor.py
@@ -8,7 +8,7 @@ Just a prototype, not meant to be particularly fast.
 """
 from sonLib.bioio import fastaRead
 from argparse import ArgumentParser
-from collections import Counter
+from collections import defaultdict
 
 def pack(word, pattern):
     """Return a packed word given a spaced seed pattern.
@@ -24,17 +24,34 @@ def pack(word, pattern):
     return "".join(ret)
 
 def yieldSpacedKmers(seq, pattern):
+    """Yield spaced k-mers (and whether they are masked) according to the match/don't care pattern.
+
+    k-mers containing Ns are skipped, and are considered masked if any bases are masked."""
     for i in xrange(len(seq) - len(pattern) + 1):
-        yield pack(seq[i:i+len(pattern)], pattern)
+        packed = pack(seq[i:i+len(pattern)], pattern)
+        # We count the k-mer as masked if any of its bases are masked.
+        masked = packed != packed.upper()
+        # Normalize the case of the packed k-mer
+        packed = packed.lower()
+        if 'n' in packed:
+            # Gap/unknown base occurs
+            continue
+        yield packed, masked
 
 def countSpacedKmers(seq, pattern):
-    """Returns a Counter object containing the count of each seed.
+    """Returns a dict from spaced k-mers to values of type:
+    [number of occurences, number of masked occurences].
 
-    >>> c = countSpacedKmers('actgactg', [True, False, True])
-    >>> c == Counter(['at', 'cg', 'ta', 'gc', 'at', 'cg'])
+    >>> c = countSpacedKmers('aCTGactg', [True, False, True])
+    >>> c == {'at': [2, 2], 'cg': [2, 1], 'ta': [1, 1], 'gc': [1, 1]}
     True
     """
-    return Counter(yieldSpacedKmers(seq, pattern))
+    counts = defaultdict(lambda: [0, 0])
+    for kmer, masked in yieldSpacedKmers(seq, pattern):
+        counts[kmer][0] += 1
+        if masked:
+            counts[kmer][1] += 1
+    return counts
 
 def parse_pattern(pattern):
     """Convert a string of 1s and 0s into a pattern of Trues and Falses.
@@ -53,20 +70,20 @@ def parse_args():
 
 def main():
     opts = parse_args()
-    count = Counter()
+    count = defaultdict(lambda: [0, 0])
     for header, seq in fastaRead(opts.fasta):
-        print 'read'
-        seq = seq.lower()
-        print 'lowered'
-        seq = seq.translate(None, 'n')
-        print 'filtered'
-        kmers = countSpacedKmers(seq, opts.pattern)
-        print header, kmers.most_common(10)
-        count += kmers
-    print count.most_common(10)
+        # Add in the counts for this sequence.
+        # For speed we should obviously just be passing the count
+        # dictionary in rather than adding to it later, but this way is simpler.
+        for kmer, v in countSpacedKmers(seq, opts.pattern).iteritems():
+            count[kmer][0] += v[0]
+            count[kmer][1] += v[1]
     with open('dump', 'w') as f:
-        for key, val in count.items():
-            f.write('%s\t%s\n' % (key, val))
+        f.write('seed\tnumOccurrences\tnumMaskedOccurrences\n')
+        for key, val in count.iteritems():
+            occurrences = val[0]
+            maskedOccurrences = val[1]
+            f.write('%s\t%s\t%s\n' % (key, occurrences, maskedOccurrences))
 
 if __name__ == '__main__':
     main()
