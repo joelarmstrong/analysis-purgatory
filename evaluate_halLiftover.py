af04fc9bd734d9f3604a975aecb25bbae0742650
Joel Armstrong <jcarmstr@ucsc.edu>
2018-09-17 19:04:24 -0700

    evalutateLiftOver changes to avoid collapse

--- evaluate_halLiftover.py
+++ evaluate_halLiftover.py
@@ -9,6 +9,7 @@ NUM_SAMPLES = 5
 parser = ArgumentParser()
 parser.add_argument('hal', default="/mnt/ntfs/RC_checkOutgroupInclusion_limit500k_10iter.hal")
 parser.add_argument('--no-perf', action='store_true')
+parser.add_argument('--no-collapse', action='store_true', help="don't collapse self-recursive functions")
 opts = parser.parse_args()
 
 times = []
@@ -26,5 +27,25 @@ if not opts.no_perf:
     # Another run to generate a flamegraph. This is separate from the
     # other runs to avoid any potential effects of perf overhead
     # (though they should be minuscule).
-    check_call("perf record -F 100 -g --call-graph dwarf -- halLiftover %s human human.bed mouse mouse.bed" % opts.hal, shell=True)
-    check_call("perf script | stackcollapse-perf.pl | flamegraph.pl /dev/stdin > halLiftover.svg", shell=True)
+    check_call("perf record -F 612 -g --call-graph dwarf -- halLiftover %s human human.bed mouse mouse.bed" % opts.hal, shell=True)
+    check_call("perf script | stackcollapse-perf.pl > halLiftover.stacks", shell=True)
+    with open('halLiftover.stacks') as input, open('halLiftover.stacks.merged', 'w') as output:
+        for line in input:
+            stacks = line.strip().split(';')
+            try:
+                i = stacks.index('hal::BlockLiftover::liftInterval')
+            except ValueError:
+                pass
+            else:
+                stacks = stacks[i:]
+            if not opts.no_collapse:
+                # Collapse recursive runs of the same function into a single instance.
+                new_stacks = []
+                prev_entry = None
+                for entry in stacks:
+                    if entry != prev_entry:
+                        new_stacks.append(entry)
+                    prev_entry = entry
+                stacks = new_stacks
+            output.write(";".join(stacks) + "\n")
+    check_call("flamegraph.pl halLiftover.stacks.merged > halLiftover.svg", shell=True)
