159d578379c9c8d2e2a734727331e6b64d40264b
Joel Armstrong <jcarmstr@ucsc.edu>
2019-02-05 20:25:40 -0800

    Misc changes to include chrom_sizes

--- c2h-bug-finder/src/main.rs
+++ c2h-bug-finder/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 use std::env;
 use std::fmt;
 use std::fs::File;
@@ -13,15 +13,19 @@ fn main() {
     let subcommand = argv.get(1).expect("Must have 2 arguments (find/fix and c2h file to check)");
     let path = argv.get(2).expect("Must have 2 arguments (find/fix and c2h file to check)");
     let reader = BufReader::new(File::open(path).expect("Can't open input file"));
+    let chrom_sizes_path = argv.get(3).expect("Must provide chrom.sizes file as 3rd argument");
+    let chrom_sizes = read_chrom_sizes(chrom_sizes_path).expect("couldn't read chrom.sizes");
     match subcommand.as_ref() {
-        "find" => find(reader, path),
-        "fix" => fix(reader, &mut io::stdout().lock()).expect("fix failed"),
+        "find" => find(reader, &chrom_sizes, path),
+        "fix" => {
+            fix(reader, &chrom_sizes, &mut io::stdout().lock()).expect("fix failed")
+        },
         _ => panic!("Only find and fix are valid subcommnands"),
     }
 }
 
-fn find(input: impl Read, path: &str) {
-    match find_buggy_lines(input) {
+fn find(input: impl Read, chrom_sizes: &HashMap<String, u64>, path: &str) {
+    match find_buggy_lines(input, chrom_sizes) {
         Ok(bugs) => {
             if !bugs.is_empty() {
                 eprintln!("Found {} bugs in file {}, lines:", bugs.len(), path);
@@ -48,6 +52,19 @@ use self::C2HData::*;
 struct C2HLine {
     line_num: u64,
     data: C2HData,
+}
+
+fn read_chrom_sizes(path: &str) -> Fallible<HashMap<String, u64>> {
+    let mut chrom_sizes = HashMap::new();
+    let rdr = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .delimiter(b'\t')
+        .from_path(path)?;
+    for result in rdr.into_records() {
+        let record = result?;
+        chrom_sizes.insert(record[0].into(), record[1].parse()?);
+    }
+    Ok(chrom_sizes)
 }
 
 impl fmt::Display for C2HData {
@@ -123,25 +140,33 @@ fn fix_and_output(buf: &mut Vec<C2HLine>, start_after_bad_region: u64, output: &
     Ok(removed)
 }
 
-fn fix(input: impl Read, output: &mut impl Write) -> Fallible<()> {
+fn fix(input: impl Read, chrom_sizes: &HashMap<String, u64>, output: &mut impl Write) -> Fallible<()> {
     let mut buf: Vec<C2HLine> = vec![];
-    let mut prev_start = 0;
+    let mut prev_end = 0;
     let mut removed_segments = HashSet::new();
+    let mut cur_seq_length = None;
     for result in get_iter(input)? {
         let c2h_line = result?;
-        match c2h_line.data {
-            BottomSegment { start, .. } | TopSegment { start, .. } => {
-                if start < prev_start {
+        match &c2h_line.data {
+            BottomSegment { start, length, .. } | TopSegment { start, length, .. } => {
+                if *start < prev_end {
                     // End of problem region. Go back through and fix what needs to be fixed.
-                    removed_segments.extend(fix_and_output(&mut buf, start, output)?);
+                    println!("problem region found at {}", *start);
+                    removed_segments.extend(fix_and_output(&mut buf, *start, output)?);
                 }
-                prev_start = start;
+                prev_end = start + length;
             },
-            NewSequence { .. } => {
-                prev_start = 0;
+            NewSequence { header, .. } => {
+                if let Some(&seq_length) = cur_seq_length {
+                    if seq_length != prev_end {
+                        removed_segments.extend(fix_and_output(&mut buf, seq_length, output)?);
+                    }
+                }
                 for item in buf.drain(..) {
                     write!(output, "{}", item.data)?;
                 }
+                prev_end = 0;
+                cur_seq_length = chrom_sizes.get(&header[1..header.len() - 1]);
             }
         }
         if let TopSegment { name: Some(name), .. } = c2h_line.data {
@@ -190,9 +215,10 @@ fn get_iter(input: impl Read) -> Fallible<impl Iterator<Item = Fallible<C2HLine>
     }))
 }
 
-fn find_buggy_lines(input: impl Read) -> Fallible<Vec<u64>> {
+fn find_buggy_lines(input: impl Read, chrom_sizes: &HashMap<String, u64>) -> Fallible<Vec<u64>> {
     let mut ret = vec![];
     let mut cur_pos = 0;
+    let mut cur_seq_length: Option<&u64> = None;
     for result in get_iter(input)? {
         let c2h = result?;
         match c2h.data {
@@ -202,7 +228,15 @@ fn find_buggy_lines(input: impl Read) -> Fallible<Vec<u64>> {
                 }
                 cur_pos = start + length;
             },
-            NewSequence { .. } => cur_pos = 0,
+            NewSequence { header, .. } => {
+                if let Some(cur_seq_length) = cur_seq_length {
+                    if cur_pos != *cur_seq_length {
+                        ret.push(c2h.line_num);
+                    }
+                }
+                cur_seq_length = chrom_sizes.get(&header);
+                cur_pos = 0;
+            },
         }
     }
     Ok(ret)
@@ -228,7 +262,7 @@ a	4183562578850463927	761	381
 a	4799148352916656457	381	2
 ";
         let mut output = vec![];
-        fix(input.as_bytes(), &mut output).unwrap();
+        fix(input.as_bytes(), &HashMap::new(), &mut output).unwrap();
         assert_eq!(from_utf8(&output).unwrap(), "s	'foo'	'bar'	0
 a	0	0	327
 a	5572078638964173662	327	13
@@ -250,7 +284,7 @@ a	6533315684431011821	3060651	1020222
 a	7043489079719062260	1020222	71
 ";
         let mut output = vec![];
-        fix(input.as_bytes(), &mut output).unwrap();
+        fix(input.as_bytes(), &HashMap::new(), &mut output).unwrap();
         assert_eq!(from_utf8(&output).unwrap(), "a	0	0	1020209
 a	7043489079719062257	1020209	3
 a	6533315684431011826	1020212	5
@@ -283,7 +317,7 @@ a	2900458897514937153	43523	13
 a	2900458897514937156	43536	14
 ";
         let mut output = vec![];
-        fix(input.as_bytes(), &mut output).unwrap();
+        fix(input.as_bytes(), &HashMap::new(), &mut output).unwrap();
         assert_eq!(from_utf8(&output).unwrap(), "a	2900458897514937132	0	43425
 a	2900458897514937132	43425	8
 a	2900458897514937135	43433	7
@@ -342,7 +376,7 @@ a	4183562578850455569	5656	831
 a	5176324821708937226	831	12
 a	7571113923563180379	843	351";
         let mut output = vec![];
-        fix(input.as_bytes(), &mut output).unwrap();
+        fix(input.as_bytes(), &HashMap::new(), &mut output).unwrap();
         assert_eq!(from_utf8(&output).unwrap(), "s	'birdAnc248'	'birdAnc248refChr2611'	1
 a	2633057669639824667	0	6
 a	8856750879689135289	6	4
@@ -369,6 +403,79 @@ a	4183562578850455571	830	1
 a	5176324821708937226	831	12
 a	7571113923563180379	843	351
 ");
+
+        let input = "s	'birdAnc64'	'birdAnc64refChr210'	1
+a	7663859928389344295	0	142
+a	3460734838657496896	142	24
+a	3460734838657496893	166	6
+a	3460734838657496890	172	48
+a	3460734838657496887	220	160
+a	3460734838657496884	380	126
+a	3460734838657496881	506	33
+a	3460734838657496878	539	98
+a	3460734838657496875	637	41
+a	3460734838657496872	678	11
+a	7663859928389344298	689	9
+a	1041598151317810649	698	5
+a	1041598151317810652	703	5
+a	1930073915304958345	708	18
+a	1041598151317810646	726	6
+a	1041598151317810644	732	732
+a	4197777065174346908	1464	18
+a	4197777065174346906	1482	750
+a	1041598151317810643	750	7
+a	1930073915304958342	757	90
+a	1041598151317810640	847	4
+a	1041598151317810638	851	851
+a	4197777065174346905	1702	2
+a	4197777065174346903	1704	853
+a	1041598151317810637	853	2
+a	1041598151317810634	855	16
+a	1930073915304958339	871	143
+a	1041598151317810631	1014	23
+a	1041598151317810628	1037	32
+a	1041598151317810626	1069	1069
+a	4197777065174346902	2138	10
+a	4197777065174346900	2148	1079
+a	1041598151317810625	1079	2
+a	1041598151317810623	1081	1081
+a	4197777065174346899	2162	1
+a	4197777065174346897	2163	1082
+a	1041598151317810622	1082	39
+a	1041598151317810620	1121	1121
+a	4197777065174346896	2242	2
+a	4197777065174346894	2244	1123
+a	1041598151317810619	1123	13
+a	1041598151317810617	1136	1136
+a	4197777065174346893	2272	1
+a	4197777065174346891	2273	1137
+a	1041598151317810616	1137	13
+a	1041598151317810614	1150	1150
+a	4197777065174346890	2300	1
+a	4197777065174346888	2301	1151
+a	1041598151317810613	1151	109
+a	1041598151317810610	1260	17
+a	1930073915304958336	1277	40
+a	1041598151317810607	1317	26
+a	1930073915304958333	1343	134
+a	1041598151317810604	1477	14
+a	1930073915304958330	1491	24
+a	1930073915304958328	1515	1515
+a	4197777065174346873	3030	25
+a	4197777065174346871	3055	1540
+a	4197777065174346876	4595	4
+a	5245849140956493994	4599	38
+a	5245849140956493991	4637	49
+a	4197777065174346879	6143	13
+a	4197777065174346877	6156	1644
+a	4197777065174346882	7800	91
+a	4197777065174346880	7891	1735
+s	'blah'	'foo'	1";
+        let mut output = vec![];
+        let mut map = HashMap::new();
+        map.insert("birdAnc64refChr210".to_owned(), 1735);
+        fix(input.as_bytes(), &map, &mut output).unwrap();
+        assert_eq!(from_utf8(&output).unwrap(), "");
     }
 
     /// Ensure a lost segment that is used later on causes a crash.
@@ -387,7 +494,7 @@ a	1234	380	4799148352916656452	1
 a	1614	1	4183562578850463929	1
 ";
         let mut output = vec![];
-        let result = fix(input.as_bytes(), &mut output);
+        let result = fix(input.as_bytes(), &HashMap::new(), &mut output);
         assert!(result.is_err());
     }
 }
